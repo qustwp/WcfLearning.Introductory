eed1ec22402514843e403dc0ed47739ad71e49ac
qustwp <695209671@qq.com>
2017-08-22 17:12:17 +0800

    优化

--- WcfLearning.Introductory.Client_SvcUtil/SvcUtilGenerated/CalculatorServiceClient.cs
+++ WcfLearning.Introductory.Client_SvcUtil/SvcUtilGenerated/CalculatorServiceClient.cs
@@ -37,26 +37,21 @@ public interface ICalculatorChannel : ICalculator, System.ServiceModel.IClientCh
 [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
 public partial class CalculatorClient : System.ServiceModel.ClientBase<ICalculator>, ICalculator
 {
-    
     public CalculatorClient()
     {
     }
-    
     public CalculatorClient(string endpointConfigurationName) : 
             base(endpointConfigurationName)
     {
     }
-    
     public CalculatorClient(string endpointConfigurationName, string remoteAddress) : 
             base(endpointConfigurationName, remoteAddress)
     {
     }
-    
     public CalculatorClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
             base(endpointConfigurationName, remoteAddress)
     {
     }
-    
     public CalculatorClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
             base(binding, remoteAddress)
     {
